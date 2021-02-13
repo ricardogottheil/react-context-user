241cf7a0d6c64c735674c3577275781843818c63
Ricardo Gottheil <ricardogottheil@gmail.com>
2021-02-13 16:26:18 -0500

    React Context State example finished

---
+++ src/components/Profile.js
@@ -0,0 +1,27 @@
+import React, { useContext } from 'react';
+import UserContext from '../context/user/UserContext';
+
+const Profile = () => {
+  const { selectedUser } = useContext(UserContext);
+
+  return (
+    <>
+      {selectedUser ? (
+        <div className='card card-body text-center '>
+          <img
+            src={selectedUser.avatar}
+            className='card-img-top rounded-circle m-auto img-fluid'
+            style={{ width: 180 }}
+            alt=''
+          />
+          <h1>{` ${selectedUser.first_name} ${selectedUser.last_name}`}</h1>
+          <h3>{selectedUser.email}</h3>
+        </div>
+      ) : (
+        <h1>No user selected</h1>
+      )}
+    </>
+  );
+};
+
+export default Profile;
